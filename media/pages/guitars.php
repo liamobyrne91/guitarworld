130cfdda0d65a47c02bcf2009b12b9c311b3c660
Liam O'Byrne <108347646+liamobyrne91@users.noreply.github.com>
2022-10-18 16:05:57 +0100

    Create guitars.php

---
+++ media/pages/guitars.php
@@ -0,0 +1,51 @@
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="description" content="As part of the Interactive Media: Graded Unit 2 (FP25 35) unit, I developed an e-commerce web application, specialising in electric and acoustic guitars. I used HTML5, CSS3, JavaScript, MySQL and PHP to develop the web application. The aim of the project was to demonstrate the significance of server-side scripting. The successful planning, development and evaluation of the project reflected on my overall grade of an A (86%).">
+<title>GuitarWorld | Guitars</title>
+<link href="../css/style.css" rel="stylesheet" type="text/css">
+<link rel="shortcut icon" href="../images/favicon.ico" type="image/vnd.microsoft.icon" />
+<link rel="icon" href="../images/favicon.ico" type="image/vnd.microsoft.icon" />
+</head>
+
+<body>
+<div id="container">
+  <header>
+    <div id="logo"></div>
+    <nav>
+      <ul>
+        <li><a href="../../index.php">Home</a></li>
+        <li><a href="guitars.php">Guitars</a>
+          <ul>
+            <li><a href="electric.php">Electric</a></li>
+            <li><a href="acoustic.php">Acoustic</a></li>
+          </ul>
+        </li>
+        <li><a href="register.php">Register</a></li>
+      </ul>
+    </nav>
+  </header>
+  <main>
+  <div id="content1_guitars">
+    <h1>Guitars</h1>
+    <p><em class="quote">"We specialise in a wide range of electric and acoustic guitars at unbelievable prices."</em> - Liam O'Byrne</p>
+  </div>
+  <div id="content2_guitars">
+    <table class="guitars">
+      <tr>
+        <td><a href="electric.php"><img src="../images/electric_small_fender.png" alt="Electric" title="Electric"/></a></td>
+        <td><a href="acoustic.php"><img src="../images/acoustic_small_fender.png" alt="Acoustic" title="Acoustic"/></a></td>
+      </tr>
+      <tr>
+        <td><a href="electric.php">Electric</a></td>
+        <td><a href="acoustic.php">Acoustic</a></td>
+      </tr>
+    </table>
+  </div>
+  </main>
+  <footer> <em>&copy; 2011 GuitarWorld</em> </footer>
+</div>
+</body>
+</html>
